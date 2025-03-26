46def1c5aab8415603893dfba05b8e60350d7331
Emily Rodrigues <emily.rodrigues@proton.me>
2025-03-26 18:28:59 -0400

    Add ability to process entire year of data when month is not specified. Make README more informative and accurate.

--- README.md
+++ README.md
@@ -3,13 +3,18 @@
 
 An automated expense tracker for personal finance stewardship. Here's how it works:
 1. Read all bank and credit card statements (CSV) from `data/` for a specific month.
-2. Consolidate and standardize these entries into a new `data/raw.csv` file.
-3. Run the entries in `raw.csv` against those in `data/categories/`.
+2. Consolidate and standardize these entries into a `data/raw.csv` file.
+3. Run the entries in `data/raw.csv` against those in `data/categories/`.
    1. If there's a match, assign that category to the expense item in question.
    2. If there's no match, prompt the user for a category. Update the associated category file, too.
-   3. If there's a match with a multi-category merchant (e.g. Amazon), prompt the user for a category for that specific purchase.
-4. Output a `.csv` file of all categorized transactions for the month.
-5. [Optional] Use the Notion API to push each categorized entry to a Notion database, for better processing & visualization.
+4. Write categorized and/or summarized entries to CSV files or Notion database, depending on user choice.
 
 ## Requirements
-The Python module `requests` is necessary for the Notion integration to work.
+Due to the Notion integration, the program requires:
+   - The Python module `requests`. Install it with the command `python -m pip install requests`
+   - A `src/secret.py` file that defines the constants `SECRET_TOKEN` and `NOTION_DATABASE`
+
+## Command Structure
+```
+python src/main.py [o={csv,notion}] [m=<month>] [y=<year>]
+```
--- src/main.py
+++ src/main.py
@@ -13,11 +13,11 @@ def is_month_valid(input):
     try:
         month = int(input)
     except ValueError:
-        print("Please input a valid month index {1-12}.")
+        print("Please input a valid month index {1-12}.\n")
         return False
 
     if (month < 1 or month > 12):
-        print("Please input a valid month index {1-12}.")
+        print("Please input a valid month index {1-12}.\n")
         return False
 
     return True
@@ -26,14 +26,30 @@ def is_year_valid(input):
     try:
         year = int(input)
     except ValueError:
-        print("Please input a valid year.")
+        print("Please input a valid year.\n")
         return False
 
-    if (year < 1):
-        print("Please input a valid year.")
+    if (year < 1 or year > datetime.now().year):
+        print("Please input a valid year.\n")
         return False
 
     return True
+
+def process_data(month, year, mode):
+    destination = "data/expenses/{}{}.csv".format(month_name[month].lower(), year)
+
+    importer.import_all_transactions(month, year)
+    totals = categorizer.categorize_expenses(destination)
+
+    if (output == "csv"):
+        summarizer.summarize_expenses(month, year, totals, mode)
+        print("Categorized transactions for {} {} written to {}".format(month_name[month], year, destination))
+        print("Summarized transactions for {} {} written to data/summary.csv".format(month_name[month], year))
+        print()
+    else:
+        publisher.publish_transactions(destination)
+        print("Categorized transactions for {} {} published to Notion!".format(month_name[month], year))
+        print()
 
 if __name__ == "__main__":
     month = datetime.now().month
@@ -41,6 +57,7 @@ if __name__ == "__main__":
     output = "csv"
 
     input_valid = True
+    month_specified = False
 
     for arg in sys.argv[1:]:
         prefix = arg[0:2]
@@ -48,6 +65,7 @@ if __name__ == "__main__":
         if prefix == "m=":
             if is_month_valid(value) == True:
                 month = int(value)
+                month_specified = True
             else:
                 input_valid = False
         elif prefix == "y=":
@@ -59,30 +77,21 @@ if __name__ == "__main__":
             if value == "csv" or value == "notion":
                 output = value
             else:
-                print("Please input a valid output format {csv,notion}.")
+                print("Please input a valid output format {csv,notion}.\n")
                 input_valid = False
         else:
             print("main.py [o={csv,notion}] [m=<month>] [y=<year>]\n")
             print("   Defaults:\n")
             print("      o=csv")
-            print("      m=<current month>")
             print("      y=<current year>")
+            print()
             input_valid = False
             break
 
     if (input_valid == True):
-        destination = "data/expenses/{}{}.csv".format(month_name[month].lower(), year)
-
-        importer.import_all_transactions(month, year)
-        totals = categorizer.categorize_expenses(destination)
-
-        if (output == "csv"):
-            # Change "w"rite mode to "a"ppend mode if you want to build a summary for the year
-            summarizer.summarize_expenses(month, year, totals, "w")
-            print("Categorized transactions for {} {} written to {}".format(month_name[month], year, destination))
-            print("Summarized transactions for {} {} written to data/summary.csv".format(month_name[month], year))
-            print()
+        if (month_specified == True):
+            process_data(month, year, "w")
         else:
-            publisher.publish_transactions(destination)
-            print("Categorized transactions for {} {} published to Notion!".format(month_name[month], year))
-            print()
+            process_data(1, year, "w")
+            for month in range(2,13):
+                process_data(month, year, "a")
